3cd4e64500b23f25e0263b550a3ce572c40f3c7b
jiaxliu <jiaxliu@outlook.com>
2017-07-18 15:57:58 +0800

    modified with bootstrap3

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -38,9 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	
+
+    # 第三方应用程序
+    'bootstrap3',
+
 	# 我的应用程序
 	'learning_logs',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 我的设置
+LOGIN_URL = '/users/login/'
+
+# django-bootstrap3的设置
+BOOTSTRAP3 = {
+ 'include_jquery': True,
+ } 
